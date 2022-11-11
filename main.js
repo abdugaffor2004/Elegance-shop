cdbef23c0fb2fc46c711d65a270e05464005d80e
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2022-11-11 22:39:06 +0300

    feat: started adding modal window

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const menuBtn_icon = document.querySelector(".menuBtn_icon")
+const nav_list = document.querySelector('.nav_list')
+const nav_list_holder = document.querySelector('.nav_list-holder')
 
 menuBtn_icon.addEventListener("click", () => {
     menuBtn_icon.classList.toggle('active')
+
+    //  menuBtn_icon.classList.contains('active') === true ? nav_list_holder.style.display = 'block' : nav_list_holder.style.display = 'none'
+    menuBtn_icon.classList.contains('active') === true ? nav_list_holder.style.opacity = 1 : nav_list_holder.style.opacity = 0;
+
 })
