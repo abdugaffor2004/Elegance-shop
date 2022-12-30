5e2e6eb8ad0a425cf4378948aef89c4afddd69d6
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2022-12-30 20:08:23 +0300

    feat: now cross won't deseapear when side bar comes out

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuBtn_icon = document.querySelector(".menuBtn_icon")
 const nav_list = document.querySelector('.nav_list')
 const nav_list_holder = document.querySelector('.nav_list-holder')
+const menuBtn = document.querySelector('.menuBtn')
+const nav_cart = document.querySelector('.nav_cart')
 
 menuBtn_icon.addEventListener("click", () => {
     menuBtn_icon.classList.toggle('active')
@@ -9,5 +11,17 @@ menuBtn_icon.addEventListener("click", () => {
     //menuBtn_icon.classList.contains('active') === true ? nav_list_holder.style.opacity = 1 : nav_list_holder.style.opacity = 0;
 
     nav_list_holder.classList.toggle('nav_list-holder_visible')
+
+    //чтобы крестик оставался при прокрутке сайдбара
+    if(document.querySelector('.active')) {
+        menuBtn.style = "position:fixed; margin-left: 60px" ;
+        nav_cart.style = "margin-right: 35px;"
+    }
+
+    else{
+        menuBtn.style = "position:none; margin-left: 0" ;
+        nav_cart.style = "margin-right: 0px;"
+    }
+    
 
 })
